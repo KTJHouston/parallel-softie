5067783fcf87d4b34dfaf802a210737cb028ee21
ktjhouston <ktjhouston@gmail.com>
2019-10-29 15:03:55 -0500

    Create DNA class

---
+++ src/DNA.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+typedef unsigned long dna_t;
+
+class DNA
+{
+    private:
+    dna_t dna;
+
+    public:
+
+    DNA(dna_t dna)
+    {
+        this->dna = dna;
+    }
+
+    private:
+
+};
