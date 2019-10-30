198590e54af48890957ea23c4229b3bebcd26e23
ktjhouston <ktjhouston@gmail.com>
2019-10-30 00:24:53 -0500

    Separate header from implementation

--- src/DNA.cpp
+++ src/DNA.cpp
@@ -1,36 +1,19 @@
-#include <iostream>
+#include "DNA.h"
 
-using namespace std;
-
-typedef unsigned long dna_t;
-
-class DNA
-{
-    private:
-    dna_t dna;
-
-    public:
-
-    DNA(dna_t dna);
-
-    //readable_coat_length
-    //readable_stiffness
-    //readable_background_color
-    //readable_foreground_color
-    //readable_paw_and_tail
-    //readable_tail_leangth_and_shape
-    //readable_weight
-    //readable_pawprint_area
-    //readable_webbing
-    //readbale_temper
-    //to_string
-
-    private:
-
-};
+/////////////////////////////////////////////////////////////////
+// PUBLIC
+/////////////////////////////////////////////////////////////////
 
 DNA::DNA(dna_t dna)
 {
     this->dna = dna;
 }
 
+{
+
+
+
+
+
+
+}
---
+++ src/DNA.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+typedef unsigned long dna_t;
+
+class DNA
+{
+    private:
+    dna_t dna;
+
+    public:
+
+    DNA(dna_t dna);
+
+    string readable_coat_length();
+    //readable_stiffness
+    //readable_background_color
+    //readable_foreground_color
+    //readable_paw_and_tail
+    //readable_tail_leangth_and_shape
+    //readable_weight
+    //readable_pawprint_area
+    //readable_webbing
+    //readbale_temper
+    //to_string
+
+    private:
+
+    string get_binary_representation(int value, int bit_count);
+
+};
