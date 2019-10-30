9a68de60d39ebbb3945d33484a924a61d8789fa5
ktjhouston <ktjhouston@gmail.com>
2019-10-29 23:26:05 -0500

    Outline DNA parts

--- src/DNA.cpp
+++ src/DNA.cpp
@@ -16,6 +16,18 @@ class DNA
         this->dna = dna;
     }
 
+    //readable_coat_length
+    //readable_stiffness
+    //readable_background_color
+    //readable_foreground_color
+    //readable_paw_and_tail
+    //readable_tail_leangth_and_shape
+    //readable_weight
+    //readable_pawprint_area
+    //readable_webbing
+    //readbale_temper
+    //to_string
+
     private:
 
 };
