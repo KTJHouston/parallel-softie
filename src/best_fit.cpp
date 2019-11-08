b542300181a4fbaf3a9873bc01cc6dc973f19b96
Kenzie Clarke <kenzie.clarke@tcu.edu>
2019-11-08 14:56:12 -0600

    Create best_fit.cpp

---
+++ src/best_fit.cpp
@@ -0,0 +1,32 @@
+
+#include "DNA.h"
+
+using namespace std;
+
+/***
+ * best_fit function
+ * 
+ * designed to rate a dog's DNA against
+ * the DNA of the desired 'softie' type
+ * 
+ * coat_length >= 8 inches
+ * stiffness = extremely soft (or even better ultimate softness)
+ * background color = white
+ * foreground color = brown
+ * paw color = white
+ * tail color = black
+ * tail length and shape = 4 to 6 inches, pointing straight up
+ * weight >= 90 kg
+ * pawprint area > 9 sq inches
+ * webbing = fully webbed between toes
+ * temper = extremely mild tempered
+ *
+ * Any dog with a float of 1.0 or higher is considered a softie. Each
+ * characteristic is responsible for 0.1 of the total evaluation
+ * 
+ * ***/   
+float best_fit(DNA dog) {
+    float eval = 0.0;
+
+    return 0.0;
+}
