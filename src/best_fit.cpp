2ac5237137165a911f6f92e4ce14d26512571983
Kenzie Clarke <kenzie.clarke@tcu.edu>
2019-11-15 09:44:25 -0600

    Added documentation
    Added method descriptions for aux methods

--- src/best_fit.cpp
+++ src/best_fit.cpp
@@ -21,12 +21,11 @@ float eval_tail_color(string t_color);
  * designed to rate a dog's DNA against
  * the DNA of the desired 'softie' type
  * 
- * coat_length >= 8 inches
- * stiffness = extremely soft (or even better ultimate softness)
- * background color = white
- * foreground color = brown
- * paw color = white
- * tail color = black
+ * There are 10 characteristics: coat length, stiffness,
+ * coat background color, foreground color, paw color, tail color,
+ * tail length (and shape), weight, pawprint area, webbing, and temper
+ * 
+ * TODO: remove once all aux methods are created
  * tail length and shape = 4 to 6 inches, pointing straight up
  * weight >= 90 kg
  * pawprint area > 9 sq inches
@@ -34,7 +33,7 @@ float eval_tail_color(string t_color);
  * temper = extremely mild tempered
  *
  * Any dog with a float of 1.0 or higher is considered a softie. Each
- * characteristic is responsible for 0.1 of the total evaluation
+ * characteristic is responsible for 0.1 of the total evaluation.
  * 
  * ***/   
 float best_fit(DNA dog) {
@@ -53,6 +52,19 @@ float best_fit(DNA dog) {
     return eval;
 }
 
+/***
+ * eval_coat_length function
+ * 
+ * evaluates the dog's coat length to see how close it
+ * is to the softie
+ * 
+ * Desirable softie : 8 inches or longer
+ * Close but not quite : 4 - 8 inches
+ * 
+ * @returns float
+ * @param int coat_length
+ * 
+ ***/
 float eval_coat_length(int coat_length) { 
     if(coat_length >= 8) {
         return 0.1;
@@ -65,6 +77,19 @@ float eval_coat_length(int coat_length) {
     }
 }
 
+/***
+ * eval_stiffness function
+ * 
+ * evaluates the dog's coat stiffness to see how close it
+ * is to the softie
+ * 
+ * Desirable softie : Ultimate softness, Extremely soft
+ * Close but not quite : More stiff than desirable
+ * 
+ * @returns float
+ * @param string stiffness
+ * 
+ ***/
 float eval_stiffness(string stiffness) {
     if(stiffness.find("Extremely soft") == 0 || stiffness.find("Ultimate Softness") == 0) {
         return 0.1;
@@ -78,6 +103,18 @@ float eval_stiffness(string stiffness) {
     
 }
 
+/***
+ * eval_bg_color function
+ * 
+ * evaluates the dog's coat background color
+ * to see how close it is to the softie
+ * 
+ * Desirable softie : white (background)
+ * 
+ * @returns float
+ * @param string bg_color
+ * 
+ ***/
 float eval_bg_color(string bg_color) {
     //white background
     if(bg_color.find("white") == 0) {
@@ -89,6 +126,19 @@ float eval_bg_color(string bg_color) {
   
 }
 
+
+/***
+ * eval_fg_color function
+ * 
+ * evaluates the dog's coat foreground color
+ * to see how close it is to the softie
+ * 
+ * Desirable softie : brown (spots)
+ * 
+ * @returns float
+ * @param string fg_color
+ * 
+ ***/
 float eval_fg_color(string fg_color) {
     //brown spots
     if(fg_color.find("brown") == 0){
@@ -100,6 +150,19 @@ float eval_fg_color(string fg_color) {
 
 }
 
+
+/***
+ * eval_paw_color function
+ * 
+ * evaluates the dog's paw color
+ * to see how close it is to the softie
+ * 
+ * Desirable softie : white
+ * 
+ * @returns float
+ * @param string p_color
+ * 
+ ***/
 float eval_paw_color(string p_color) {
     string paw_snip = p_color.substr(0,p_color.find_first_of("paws"));
     if(paw_snip.find("white") == 0) {
@@ -110,6 +173,19 @@ float eval_paw_color(string p_color) {
     }
 }
 
+
+/***
+ * eval_tail_color function
+ * 
+ * evaluates the dog's tail color
+ * to see how close it is to the softie
+ * 
+ * Desirable softie : black
+ * 
+ * @returns float
+ * @param string t_color
+ * 
+ ***/
 float eval_tail_color(string t_color) {
     string tail_snip = t_color.substr(t_color.find_first_of("paws"), t_color.end);
     if(tail_snip.find("black") == 0) {
@@ -120,8 +196,8 @@ float eval_tail_color(string t_color) {
     }
 }
 
-//float eval_t_len_and_shape(int length, string shape);
-//float eval_weight(int weight);
-//float eval_pp_area(int sq_inches);
-//float eval_webbing(string webbing);
-//float eval_temper(string temper);
+// TODO: float eval_t_len_and_shape(int length, string shape);
+// TODO: float eval_weight(int weight);
+// TODO: float eval_pp_area(int sq_inches);
+// TODO: float eval_webbing(string webbing);
+// TODO: float eval_temper(string temper);
