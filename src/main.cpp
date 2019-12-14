31350a96145fd31ed5a703cc4372ecb024c0f094
KTJHouston <ktjhouston@gmail.com>
2019-12-14 15:59:01 -0600

    Define multi_island_sim

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,23 @@ using namespace std;
 
 int check_parents(vector<DNA> parents);
 int single_island_sim(int size, bool verbose);
+int multi_island_sim(int size, int n, int trade_freq, bool verbose);
 float multi_percent_softie(vector<Island> islands);
 
 int main(int argc, char** argv) {
-    //run sim 100 times:
-    for (int i = 0; i < 100; i++) {
-        int gen = single_island_sim(500, false);
+    int num_sim = 500;
+
+    //run single sim 100 times:
+    cout << "Single sims:" << endl;
+    for (int i = 0; i < num_sim; i++) {
+        int gen = single_island_sim(100, false);
     }
-    //cout << "Gen " << gen << endl;
+
+    //run multi sim once:
+    cout << "Multi sims:" << endl;
+    for (int i = 0; i < num_sim; i++) {
+        int gen = multi_island_sim(100, 5, 5, false);
+    }
 }
 
 /**
@@ -95,6 +104,75 @@ int single_island_sim(int size, bool verbose) {
 
     return gen_cnt;
 }//end single island sim
+
+int multi_island_sim(int size, int n, int trade_freq, bool verbose) {
+    //start timer:
+    auto t_start = std::chrono::high_resolution_clock::now();
+
+    //init island:
+    vector<Island> islands;
+    for (int i = 0; i < n; i++) {
+        islands.push_back(Island(size));
+    }
+    int gen_cnt = 0;
+
+    while ( multi_percent_softie(islands) < .1 ) {
+        //find parents:
+        vector<vector<DNA>> parents;
+        for (int i = 0; i < islands.size(); i++) {//for each island
+            //find parents:
+            parents.push_back(islands[i].find_parents());
+        }
+
+        //trade parents if time:
+        if ( gen_cnt % trade_freq == 0 ) {
+            vector<vector<DNA>> new_parents;
+            for (int i = 0; i < islands.size(); i++) {//for each island
+                //take parents from others:
+                new_parents.push_back(vector<DNA>(2));
+                new_parents[i][0] = parents[(i-1)%islands.size()][1];
+                new_parents[i][1] = parents[(i+1)%islands.size()][0];
+            }
+            parents = new_parents;
+        }
+
+        //set parents and breed:
+        for (int i = 0; i < islands.size(); i++) {//for each island
+            //set parents:
+            islands[i].set_parents(parents[i]);
+
+            //breed from parents:
+            islands[i].breed();
+        }
+
+        //increase generation counter:
+        gen_cnt++;
+
+        //print ratings:
+        /*
+        if ( verbose ) {
+            for (int i = 0; i < islands.size(); i++) {//for each island
+                cout << "Gen " << gen_cnt << endl;
+                for (int i = 0; i < parents.size(); i++) {
+                    cout << "Rating: " << rate(parents[i]) << endl;
+                cout << DNA::to_string(parents[i]) << endl << endl;
+                }
+                cout << "Softie Percent: " << island.percent_softie() << endl;
+                cout << endl;
+            }
+        }*/
+    }
+
+    //end timer:
+    auto t_end = std::chrono::high_resolution_clock::now();
+
+    //print time to complete simulation:
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t_end - t_start ).count();
+    //cout << "Multi island speed: " << duration << " microseconds" << endl;
+    cout << gen_cnt << "," << duration << endl;
+
+    return gen_cnt;
+}//end multi island sim
 
 /**
  * multi_percent_softie function
